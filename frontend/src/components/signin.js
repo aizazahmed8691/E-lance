687a03c0dda9e55fccc599cc229fe5d19691251c
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-02-14 23:18:55 +0500

    login and signup

--- frontend/src/components/signin.js
+++ frontend/src/components/signin.js
@@ -1,41 +1,72 @@
 import React, {useState,useEffect} from "react";
 
-// import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import {Form, Button} from 'react-bootstrap'
 const SigninForm = () => {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
 //  const dispatch = useDispatch();
 //  const userogin =  useSelector(state=>state.userlogin)
 //  const {loading,error,userinfo} = userogin;
-//  let history = useNavigate();
+ let history = useNavigate();
 
-//  useEffect(()=>{
-//     if(userinfo){
-//         if(userinfo.isBuyer===true){
-//             history('/user-buyer')
-//         }
-//         else{
-//             history('/user')
-//         }
-//     }
-//  },[history,userinfo])
+ useEffect(()=>{
+   const user= localStorage.getItem('userInfo')
+    if(user){
+        if(user.buyer){
+            history('/buyer')
+        }else{
+            history('/seller')
+        }
+    }
+    },[history])
 
-//  const submitHandler = (e) =>{
-//     e.preventDefault()
-//     // dispatch(login(email,password))
-//  }
+ const submitHandler = (e) =>{
+    e.preventDefault()
+    // dispatch(login(email,password))
+    fetch("http://localhost:5000/user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            localStorage.setItem('userInfo',JSON.stringify(data))
+            if(data.buyer){
+                history('/buyer')
+            }else{
+                history('/seller')
+            }
+        }
+        );
+        
+ }
     return (
         <div>
-            <Form>
+            <Form
+            onSubmit={submitHandler}
+            >
                 <h4>Sign in to Elance</h4>
                 <Form.Group
                 controlId="fromBasicEmail">
-                    <Form.Control type="email" placeholder="Email/Username" />
+                    <Form.Control type="email" placeholder="Email/Username" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
                 </Form.Group>
                 <Form.Group
                 controlId="fromBasicPassword">
-                    <Form.Control type="password" placeholder="Password"/>
+                    <Form.Control type="password" placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
                 </Form.Group>
                 {/* {loading && <h5>....loading</h5>}
                 {error && <h5>{error}</h5>} */}
--- frontend/src/pages/join.js
+++ frontend/src/pages/join.js
@@ -1,23 +1,64 @@
 import React from "react";
 import {Form,Button} from 'react-bootstrap';
 const JoinForm = () => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [userName, setUserName] = React.useState("");
+    const [buyer, setbuyer] = React.useState(false);
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        const data = new FormData();
+        data.append("email", email);
+        data.append("password", password);
+        data.append("userName", userName);
+        data.append("buyer", buyer);
+        fetch("http://localhost:5000/user/register", {
+            method: "POST",
+            body: data,
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            }
+            );
+    }
+
+
     return (
         <div className="logonform">
-            <Form>
+            <Form
+                onSubmit={SubmitHandler}
+            >
                 <h3>Sign in to Elance</h3>
                 <br></br>
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Email"/>
+                    <Form.Control type="email" placeholder="Email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+
+                    />
                 </Form.Group>
                 <Form.Group controlId="formBasicName">
-                    <Form.Control type="Name" placeholder="Choose a Username"/>
+                    <Form.Control type="Name" placeholder="Choose a Username"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+
+                    />
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Set a Password"/>
+                    <Form.Control type="password" placeholder="Set a Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+
+                    />
                 </Form.Group>
                 <Form.Group controlId="formBasicName">
                     <div className="nbv">
-                    <Form.Check custom type="checkbox" id="checkbox9" placeholder="Choose a Username"/>
+                    <Form.Check custom type="checkbox" id="checkbox9" placeholder="Choose a Username"
+                    // value={buyer}
+                    onChange={(e) => setbuyer(!buyer)}
+
+                    />
                     <p>Join as a Buyer</p>
                     </div>
                     <Button variant="success" size="md" block type="submit">Submit</Button>
